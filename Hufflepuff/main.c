584912f8130506a69326861002523bf0e3b78408
BlueFrimFrim <tds1842@gmail.com>
2016-11-27 17:54:15 -0700

    /

---
+++ Hufflepuff/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Hufflepuff
+//
+//  Created by Keegan Ritchie on 2016-11-27.
+//  Copyright © 2016 Keegan Ritchie. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+        // insert code here...
+        printf("Hello, World!\n");
+    return 0;
+}
